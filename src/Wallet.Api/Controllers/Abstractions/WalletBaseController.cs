a9027b4de7ca76faa4904e09f9df2bcb0cabecea
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-12-26 13:14:00 +0400

    x

---
+++ src/Wallet.Api/Controllers/Abstractions/WalletBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.Api.Controllers.Abstractions;
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class WalletBaseController : ControllerBase
+{
+    
+}
---
+++ src/Wallet.Api/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wallet.Api.Controllers.Abstractions;
+namespace Wallet.Api.Controllers;
+
+
+public class TestController : WalletBaseController
+{
+    public async Task<IActionResult> Get()
+    {
+        return Ok(new {Name = "Hello world!"});
+    }
+}
